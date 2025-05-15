b866eb37e6e8ae22bc13322b12d545ae842ae31a
Lourival Pinheiro <lourivaltpneto@outlook.com>
2025-05-14 22:27:24 -0300

    Update 2_🗄️_Projetos.py

--- pages/2_🗄️_Projetos.py
+++ pages/2_🗄️_Projetos.py
@@ -51,28 +51,3 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-
-# Footer
-footer = """
-<style>
-/* Hide default Streamlit footer */
-footer {visibility: hidden;}
-
-.footer-custom {
-    position: relative;
-    bottom: 0;
-    width: 100%;
-    text-align: center;
-    font-size: 14px;
-    color: #ffff;
-    padding: 10px 0;
-    margin-top: auto;
-}
-</style>
-
-<div class="footer-custom">
-    © Developed by <strong>Neto Pinheiro<strong/>
-</div>
-"""
-
-st.markdown(footer, unsafe_allow_html=True)
